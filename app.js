a759b4abac5a86812cb32acf0cbd8edaca7686f3
Polina Kuznetsova <120649861+Polina-Kuznetsova-html-css@users.noreply.github.com>
2023-01-29 14:45:24 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const board = document.querySelector('#board')
+const colors = ['#00cc00', '#ff6600', '#3366ff','#ffcc00',
+'#cc0099']
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+function removeColor(element) {
+    element.style.backgroundColor = '#3b3b3b'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
